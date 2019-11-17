cd23be3dbb6a3c9320e034f6453dcaa746373cf3
PadmaBalajiL <l.padmabalaji@gmail.com>
2019-11-17 22:06:30 +0100

    Fully functional code for interfacing two sensors

---
+++ Debug/Drivers/STM32L0xx_HAL_Driver/Src/stm32l0xx_hal_dma.d
@@ -0,0 +1,89 @@
+Drivers/STM32L0xx_HAL_Driver/Src/stm32l0xx_hal_dma.o: \
+ ../Drivers/STM32L0xx_HAL_Driver/Src/stm32l0xx_hal_dma.c \
+ /home/padmabl/Desktop/EmbeddedWorks/INIR_interfacing/2ADCs/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal.h \
+ /home/padmabl/Desktop/EmbeddedWorks/INIR_interfacing/2ADCs/Inc/stm32l0xx_hal_conf.h \
+ /home/padmabl/Desktop/EmbeddedWorks/INIR_interfacing/2ADCs/Inc/main.h \
+ /home/padmabl/Desktop/EmbeddedWorks/INIR_interfacing/2ADCs/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_rcc.h \
+ /home/padmabl/Desktop/EmbeddedWorks/INIR_interfacing/2ADCs/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_def.h \
+ /home/padmabl/Desktop/EmbeddedWorks/INIR_interfacing/2ADCs/Drivers/CMSIS/Device/ST/STM32L0xx/Include/stm32l0xx.h \
+ /home/padmabl/Desktop/EmbeddedWorks/INIR_interfacing/2ADCs/Drivers/CMSIS/Device/ST/STM32L0xx/Include/stm32l073xx.h \
+ /home/padmabl/Desktop/EmbeddedWorks/INIR_interfacing/2ADCs/Drivers/CMSIS/Include/core_cm0plus.h \
+ /home/padmabl/Desktop/EmbeddedWorks/INIR_interfacing/2ADCs/Drivers/CMSIS/Include/core_cmInstr.h \
+ /home/padmabl/Desktop/EmbeddedWorks/INIR_interfacing/2ADCs/Drivers/CMSIS/Include/cmsis_gcc.h \
+ /home/padmabl/Desktop/EmbeddedWorks/INIR_interfacing/2ADCs/Drivers/CMSIS/Include/core_cmFunc.h \
+ /home/padmabl/Desktop/EmbeddedWorks/INIR_interfacing/2ADCs/Drivers/CMSIS/Device/ST/STM32L0xx/Include/system_stm32l0xx.h \
+ /home/padmabl/Desktop/EmbeddedWorks/INIR_interfacing/2ADCs/Drivers/STM32L0xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ /home/padmabl/Desktop/EmbeddedWorks/INIR_interfacing/2ADCs/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_rcc_ex.h \
+ /home/padmabl/Desktop/EmbeddedWorks/INIR_interfacing/2ADCs/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_gpio.h \
+ /home/padmabl/Desktop/EmbeddedWorks/INIR_interfacing/2ADCs/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_gpio_ex.h \
+ /home/padmabl/Desktop/EmbeddedWorks/INIR_interfacing/2ADCs/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_dma.h \
+ /home/padmabl/Desktop/EmbeddedWorks/INIR_interfacing/2ADCs/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_cortex.h \
+ /home/padmabl/Desktop/EmbeddedWorks/INIR_interfacing/2ADCs/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_adc.h \
+ /home/padmabl/Desktop/EmbeddedWorks/INIR_interfacing/2ADCs/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_adc_ex.h \
+ /home/padmabl/Desktop/EmbeddedWorks/INIR_interfacing/2ADCs/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_flash.h \
+ /home/padmabl/Desktop/EmbeddedWorks/INIR_interfacing/2ADCs/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_flash_ex.h \
+ /home/padmabl/Desktop/EmbeddedWorks/INIR_interfacing/2ADCs/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_flash_ramfunc.h \
+ /home/padmabl/Desktop/EmbeddedWorks/INIR_interfacing/2ADCs/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_i2c.h \
+ /home/padmabl/Desktop/EmbeddedWorks/INIR_interfacing/2ADCs/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_i2c_ex.h \
+ /home/padmabl/Desktop/EmbeddedWorks/INIR_interfacing/2ADCs/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_pwr.h \
+ /home/padmabl/Desktop/EmbeddedWorks/INIR_interfacing/2ADCs/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_pwr_ex.h \
+ /home/padmabl/Desktop/EmbeddedWorks/INIR_interfacing/2ADCs/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_uart.h \
+ /home/padmabl/Desktop/EmbeddedWorks/INIR_interfacing/2ADCs/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_uart_ex.h
+
+/home/padmabl/Desktop/EmbeddedWorks/INIR_interfacing/2ADCs/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal.h:
+
+/home/padmabl/Desktop/EmbeddedWorks/INIR_interfacing/2ADCs/Inc/stm32l0xx_hal_conf.h:
+
+/home/padmabl/Desktop/EmbeddedWorks/INIR_interfacing/2ADCs/Inc/main.h:
+
+/home/padmabl/Desktop/EmbeddedWorks/INIR_interfacing/2ADCs/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_rcc.h:
+
+/home/padmabl/Desktop/EmbeddedWorks/INIR_interfacing/2ADCs/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_def.h:
+
+/home/padmabl/Desktop/EmbeddedWorks/INIR_interfacing/2ADCs/Drivers/CMSIS/Device/ST/STM32L0xx/Include/stm32l0xx.h:
+
+/home/padmabl/Desktop/EmbeddedWorks/INIR_interfacing/2ADCs/Drivers/CMSIS/Device/ST/STM32L0xx/Include/stm32l073xx.h:
+
+/home/padmabl/Desktop/EmbeddedWorks/INIR_interfacing/2ADCs/Drivers/CMSIS/Include/core_cm0plus.h:
+
+/home/padmabl/Desktop/EmbeddedWorks/INIR_interfacing/2ADCs/Drivers/CMSIS/Include/core_cmInstr.h:
+
+/home/padmabl/Desktop/EmbeddedWorks/INIR_interfacing/2ADCs/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+/home/padmabl/Desktop/EmbeddedWorks/INIR_interfacing/2ADCs/Drivers/CMSIS/Include/core_cmFunc.h:
+
+/home/padmabl/Desktop/EmbeddedWorks/INIR_interfacing/2ADCs/Drivers/CMSIS/Device/ST/STM32L0xx/Include/system_stm32l0xx.h:
+
+/home/padmabl/Desktop/EmbeddedWorks/INIR_interfacing/2ADCs/Drivers/STM32L0xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+/home/padmabl/Desktop/EmbeddedWorks/INIR_interfacing/2ADCs/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_rcc_ex.h:
+
+/home/padmabl/Desktop/EmbeddedWorks/INIR_interfacing/2ADCs/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_gpio.h:
+
+/home/padmabl/Desktop/EmbeddedWorks/INIR_interfacing/2ADCs/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_gpio_ex.h:
+
+/home/padmabl/Desktop/EmbeddedWorks/INIR_interfacing/2ADCs/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_dma.h:
+
+/home/padmabl/Desktop/EmbeddedWorks/INIR_interfacing/2ADCs/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_cortex.h:
+
+/home/padmabl/Desktop/EmbeddedWorks/INIR_interfacing/2ADCs/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_adc.h:
+
+/home/padmabl/Desktop/EmbeddedWorks/INIR_interfacing/2ADCs/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_adc_ex.h:
+
+/home/padmabl/Desktop/EmbeddedWorks/INIR_interfacing/2ADCs/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_flash.h:
+
+/home/padmabl/Desktop/EmbeddedWorks/INIR_interfacing/2ADCs/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_flash_ex.h:
+
+/home/padmabl/Desktop/EmbeddedWorks/INIR_interfacing/2ADCs/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_flash_ramfunc.h:
+
+/home/padmabl/Desktop/EmbeddedWorks/INIR_interfacing/2ADCs/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_i2c.h:
+
+/home/padmabl/Desktop/EmbeddedWorks/INIR_interfacing/2ADCs/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_i2c_ex.h:
+
+/home/padmabl/Desktop/EmbeddedWorks/INIR_interfacing/2ADCs/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_pwr.h:
+
+/home/padmabl/Desktop/EmbeddedWorks/INIR_interfacing/2ADCs/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_pwr_ex.h:
+
+/home/padmabl/Desktop/EmbeddedWorks/INIR_interfacing/2ADCs/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_uart.h:
+
+/home/padmabl/Desktop/EmbeddedWorks/INIR_interfacing/2ADCs/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_uart_ex.h:
